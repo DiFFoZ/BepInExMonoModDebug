cc97f392aa4e19465eeeb07053059a218f218f09
Leonid <48765566+DiFFoZ@users.noreply.github.com>
2024-08-16 02:59:07 +0700

    Move from Cecil to method builder

---
+++ BepInExMonoModDebug/CecilEmitter.cs
@@ -0,0 +1,116 @@
+﻿using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using Mono.Cecil;
+using MonoMod.Cil;
+using MonoMod.Utils;
+using MethodAttributes = Mono.Cecil.MethodAttributes;
+using MethodImplAttributes = Mono.Cecil.MethodImplAttributes;
+using ParameterAttributes = Mono.Cecil.ParameterAttributes;
+using TypeAttributes = Mono.Cecil.TypeAttributes;
+
+namespace BepInExMonoModDebug;
+internal static class CecilEmitter
+{
+    public static void Dump(MethodDefinition md, string dumpPath, MethodBase? original = null)
+    {
+        var name = SanitizeTypeName(md.GetID(withType: false, simple: true));
+        var originalName = (original?.Name ?? md.Name).Replace('.', '_');
+
+        using var module = ModuleDefinition.CreateModule(name,
+            new ModuleParameters
+            {
+                Kind = ModuleKind.Dll,
+                ReflectionImporterProvider = MMReflectionImporter.ProviderNoDefault
+            });
+
+        var td = new TypeDefinition("", originalName,
+            TypeAttributes.Public | TypeAttributes.Abstract | TypeAttributes.Sealed | TypeAttributes.Class)
+        {
+            BaseType = module.TypeSystem.Object
+        };
+
+        module.Types.Add(td);
+
+        MethodDefinition? clone = null;
+
+        Relinker relinker = (mtp, ctx) =>
+        {
+            if (mtp == md)
+                return clone!;
+            if (mtp is MethodReference mr)
+            {
+                if (mr.FullName == md.FullName
+                 && mr.DeclaringType.FullName == md.DeclaringType.FullName
+                 && mr.DeclaringType.Scope.Name == md.DeclaringType.Scope.Name)
+                    return clone!;
+            }
+            return module.ImportReference(mtp);
+        };
+
+        clone =
+            new MethodDefinition(original?.Name ?? "_" + md.Name.Replace(".", "_"), md.Attributes, module.TypeSystem.Void)
+            {
+                MethodReturnType = md.MethodReturnType,
+                Attributes = MethodAttributes.Public | MethodAttributes.HideBySig | MethodAttributes.Static,
+                ImplAttributes = MethodImplAttributes.IL | MethodImplAttributes.Managed,
+                DeclaringType = td,
+                HasThis = false,
+                ReturnType = md.ReturnType.Relink(relinker, clone)
+            };
+        td.Methods.Add(clone);
+
+        foreach (var param in md.Parameters)
+            clone.Parameters.Add(param.Clone().Relink(relinker, clone));
+
+        var body = clone.Body = md.Body.Clone(clone);
+
+        foreach (var variable in clone.Body.Variables)
+            variable.VariableType = variable.VariableType.Relink(relinker, clone);
+
+        foreach (var handler in clone.Body.ExceptionHandlers.Where(handler => handler.CatchType != null))
+            handler.CatchType = handler.CatchType.Relink(relinker, clone);
+
+        foreach (var instr in body.Instructions)
+        {
+            var operand = instr.Operand;
+            operand = operand switch
+            {
+                ParameterDefinition param => clone.Parameters[param.Index],
+                ILLabel label => label.Target,
+                IMetadataTokenProvider mtp => mtp.Relink(relinker, clone),
+                _ => operand
+            };
+
+            instr.Operand = operand;
+        }
+
+        if (md.HasThis)
+        {
+            TypeReference type = md.DeclaringType;
+            if (type.IsValueType)
+                type = new ByReferenceType(type);
+            clone.Parameters.Insert(0,
+                new ParameterDefinition("<>_this", ParameterAttributes.None, type.Relink(relinker, clone)));
+        }
+
+        if (!Directory.Exists(dumpPath))
+        {
+            Directory.CreateDirectory(dumpPath);
+        }
+
+        using var stream = new FileStream(Path.Combine(dumpPath, $"{module.Name}.dll"), FileMode.Create, FileAccess.Write);
+        module.Write(stream);
+    }
+
+    private static string SanitizeTypeName(string typeName)
+    {
+        return new StringBuilder(typeName)
+            .Replace(":", "_")
+            .Replace(" ", "_")
+            .Replace("<", "{")
+            .Replace(">", "}")
+            .ToString();
+    }
+}
--- BepInExMonoModDebug/DebugDMDGenerator.cs
+++ BepInExMonoModDebug/DebugDMDGenerator.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.IO;
-using System.Reflection;
-using System.Text;
+﻿using System.Reflection;
 using MonoMod.Utils;
 
 namespace BepInExMonoModDebug;
@@ -15,100 +12,32 @@ internal sealed class DebugDMDGenerator : DMDGenerator<DebugDMDGenerator>
     /// </remarks>
     internal static bool ShouldDump { get; set; }
 
-    protected override MethodInfo _Generate(DynamicMethodDefinition dmd, object context)
+    public override MethodInfo _Generate(DynamicMethodDefinition dmd, object context)
     {
-        if (!ShouldDump || dmd.OriginalMethod == null)
+        if (BepInExMonoModDebugPatcher.Configuration.ShouldSaveDumpFile.Value)
         {
-            ToggleDump(false);
-            return DMDGenerator<DMDCecilGenerator>.Generate(dmd, context);
+            var generatedMethod = DMDEmitMethodBuilderGenerator.Generate(dmd, context);
+
+            if (ShouldDump)
+            {
+                ShouldDump = false;
+                DumpMethodToFile(dmd);
+            }
+
+            return generatedMethod;
         }
 
         ShouldDump = false;
-
-        ToggleDump(true);
-        MethodInfo generatedMethod;
-        try
-        {
-            generatedMethod = DMDGenerator<DMDCecilGenerator>.Generate(dmd, context);
-        }
-        catch (Exception ex)
-        {
-            BepInExMonoModDebugPatcher.Logger.LogWarning("Failed to generate debug file\n" + ex);
-
-            ToggleDump(false);
-            return DMDGenerator<DMDEmitDynamicMethodGenerator>.Generate(dmd, context);
-        }
-
-        DumpMethodToFile(dmd);
-
-        return generatedMethod;
+        return DMDEmitMethodBuilderGenerator.Generate(dmd, context);
     }
 
     private static void DumpMethodToFile(DynamicMethodDefinition dmd)
     {
-        if (!BepInExMonoModDebugPatcher.Configuration.ShouldSaveDumpFile.Value)
+        if (dmd.OriginalMethod == null)
         {
             return;
         }
 
-        try
-        {
-            var methodName = GetFullMethodName(dmd.OriginalMethod) + ".dll";
-            var dumpName = dmd.GetDumpName("Cecil") + ".dll";
-
-            var fileToDelete = Path.Combine(BepInExMonoModDebugPatcher.DumpsDirectory, methodName);
-            if (fileToDelete != null)
-            {
-                File.Delete(fileToDelete);
-            }
-
-            var dumpFile = Path.Combine(BepInExMonoModDebugPatcher.DumpsDirectory, dumpName);
-            File.Move(dumpFile, fileToDelete);
-        }
-        catch (Exception ex) // even if exception occurred (no permission to write/read) we really want to continue the patching
-        {
-            BepInExMonoModDebugPatcher.Logger.LogError("Failed to create dump file: " + ex);
-        }
-    }
-
-    private static string GetFullMethodName(MethodBase @base)
-    {
-        var sb = new StringBuilder(256);
-
-        var declaringType = @base.DeclaringType;
-        if (declaringType != null)
-        {
-            var @namespace = declaringType.Namespace;
-            if (!string.IsNullOrEmpty(@namespace))
-            {
-                sb.Append(@namespace).Append('.');
-            }
-
-            sb.Append(declaringType.Name);
-        }
-
-        if (sb.Length > 0)
-        {
-            sb.Append('_');
-        }
-
-        sb.Append(@base.Name);
-
-        foreach (var invalidChar in Path.GetInvalidFileNameChars())
-        {
-            sb.Replace(invalidChar, '_');
-        }
-
-        return sb.ToString();
-    }
-
-    private void ToggleDump(bool toggle)
-    {
-        if (!BepInExMonoModDebugPatcher.Configuration.ShouldSaveDumpFile.Value)
-        {
-            return;
-        }
-
-        Environment.SetEnvironmentVariable("MONOMOD_DMD_DUMP", toggle ? BepInExMonoModDebugPatcher.DumpsDirectory : null!);
+        CecilEmitter.Dump(dmd.Definition, BepInExMonoModDebugPatcher.DumpsDirectory, dmd.OriginalMethod);
     }
 }
